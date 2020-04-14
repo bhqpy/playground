a4a536d714d9920fe19c4eab5e1b55df0fe5d9a5
sgs-hv <varchmin@schwarmstedt.de>
2020-04-14 22:47:19 +0200

    Ausgabe Status als json

--- who_is_at_home.py
+++ who_is_at_home.py
@@ -1,17 +1,31 @@
+import datetime
 import glob
 import os.path
 import sqlite3
-import datetime
+from fastapi import FastAPI
 
 
-database = 'database/novatime.s3db'
-tools = 'tools/create*.sql'
+def makequerystring_tjk():
+    # Die Funktion baut aus Tagedatum und Novatime Abwesenheitsgründen eine Abfrage für die Tabelle
+    # Liste_Jahreskartei auf.
+    # Aus Datenschutzgründen dürfen die einzelnen Abwesenheiten nicht näher spezifiziert werden.
+    heute = todays_zeitdb_field()
+    nvtgruende = ['U', 'KR', 'GL', 'F', 'Sa', 'So']  # Abwesenheitsgründe in Novatime
+    query = "select liste.AuswNr,kt.PersNr,kt.Name,'sonstiges' from Liste_Jahreskartei as kt,Liste_Personal as liste where liste.Name=kt.name and ("
+
+    i = 0
+    for g in nvtgruende:
+        i = i + 1
+        query = query + heute + " = '" + g + "'"
+        if not (i == len(nvtgruende)):
+            query = query + " or "
+    query = query + ")"
+    return query
 
 
-def createZeitDB(func_database, func_tools):
+def create_zeitdb(func_database, func_tools):
     conn = sqlite3.connect(func_database)
     c = conn.cursor()
-    datei = []
 
     for datei in glob.glob(func_tools):
         with open(datei, 'r') as file:
@@ -21,27 +35,66 @@ def createZeitDB(func_database, func_tools):
     conn.commit()
     conn.close()
 
-def todays_zeitDB_field():
-    return datetime.date.today().strftime('D%d%m')
 
-# End of definitions
-if not os.path.isfile(database):
+def todays_zeitdb_field():
+    ret = 'kt.' + datetime.date.today().strftime('D%d%m')
+    return ret
 
-    createZeitDB(database, tools)
-else:
-    conn = sqlite3.connect(database)
-    conn.text_factory = lambda x: str(x, "latin1")
-    c = conn.cursor()
-    c.execute("select * from Liste_Tagesjournalaktuellanwesend where AuswNr>1")
 
-    anwesend = c.fetchall()
-    c.execute("select * from Liste_Tagesjournalaktuellabwesend where AuswNr>1")
-    abwesend = c.fetchall()
-    c.execute("select PersNr,Name,D2302 from Liste_Jahreskartei where PersNr > 1")
-    kartei = c.fetchall()
-    conn.close()
-    print(anwesend)
-    print(abwesend)
-    print(kartei)
-    print(todays_zeitDB_field())
-    print("To be continued ...")
+def whoisthere(database, tools):
+    if not os.path.isfile(database):
+        create_zeitdb(database, tools)
+    else:
+        conn = sqlite3.connect(database)
+        conn.text_factory = lambda x: str(x, "latin1")
+        c = conn.cursor()
+        c.execute(
+            "select dg.AuswNr,liste.PersNr,dg.Name,'Dienstgang' from Liste_TagesjournalaktuellaufDienstgang as dg,Liste_Personal as liste where dg.AuswNr>0 and dg.Name=liste.Name group by dg.AuswNr")
+        dienstgang = c.fetchall()
+        c.execute(
+            "select anw.AuswNr,liste.PersNr,anw.Name,'anwesend' from Liste_Tagesjournalaktuellanwesend as anw,Liste_Personal as liste where anw.AuswNr>0 and anw.Name=liste.Name group by anw.AuswNr")
+        anwesend = c.fetchall()
+        c.execute(
+            "select abw.AuswNr,liste.PersNr,abw.Name,'abwesend' from Liste_Tagesjournalaktuellabwesend as abw,Liste_Personal as liste where abw.AuswNr>0 and abw.Name=liste.Name group by abw.AuswNr")
+        abwesend = c.fetchall()
+        c.execute(makequerystring_tjk())
+        kartei = c.fetchall()
+        tabellen = dienstgang, anwesend, kartei, abwesend
+        pnrlist = []
+        zaehler = 0
+        statusjson = '{"user":['
+        for tab in tabellen:
+            for row in tab:
+                zaehler = zaehler + 1
+                auswnr, pnr, name, status = row
+                if len(pnr) == 6:
+                    pnr = pnr + '00'
+                if len(pnr) == 4:
+                    pnr = '15' + pnr + '00'
+                if len(pnr) == 0:
+                    pnr = '99999999'
+                    # fehler = fehler + 1
+                if (pnr in pnrlist):
+                    x = ''
+                else:
+                    pnrlist.append(pnr)
+                    x = '{"Name" : "' + name + '", "AusweisNr" :"' + auswnr + '", "PersNr" : "' + pnr + '", "Status" :' \
+                        + '"' + status + '"},'
+
+                statusjson = statusjson + x
+
+        statusjson = statusjson[:-1] + ']}'
+        return statusjson
+        conn.close()
+
+
+def main():
+
+    odatabase = 'database/novatime.s3db'
+    otools = 'tools/create*.sql'
+    d = whoisthere(odatabase, otools)
+    print(d)
+
+main()
+
+print("To be continued ...")
